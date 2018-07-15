f60f6fe32d3609a01344bf904729d547726aaa16
杨大大 <@Lenovo-PC>
2018-07-15 22:19:02 +0800

    123

---
+++ bigdata35/src/main/java/cn/yangdada/bigdata35/App.java
@@ -0,0 +1,13 @@
+package cn.yangdada.bigdata35;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
